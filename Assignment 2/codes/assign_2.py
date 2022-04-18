dfafb5e0c974a697add483d53d67658dd8385afc
Charanyash <102616426+Charanyash@users.noreply.github.com>
2022-04-18 15:58:50 +0530

    Update assign_2.py

--- Assignment 2/codes/assign_2.py
+++ Assignment 2/codes/assign_2.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 from numpy import linalg as LA
 from sympy import diff
 from sympy.abc import x,y 
+
+
+# Function f(x) defined here
 def f(x):
     if (x<4):
         fval = 4 - x
@@ -11,10 +14,10 @@ def f(x):
         fval = x - 4
         return fval
 
-
+# Function f'(x) defined here.
 def dif(x):
     if(x<4):
-        fval = diff(4-y,y)
+        fval = diff(4-y,y) # f'(x) is represented with variable y to avoid confusion. 
         return fval
     else:
         fval = diff(y-4,y)
@@ -24,12 +27,16 @@ def dif(x):
 x = np.linspace(-2,10,1000)
 fvec = np.vectorize(f)
 difvec = np.vectorize(dif)
+# Two figures are ploted to check continuity and differentiablity of f at x = 4.
+
 plt.figure(1)
 plt.plot(x,fvec(x),label='Function is continuous at x = 4')
 plt.xlabel('$x$')
 plt.ylabel('$y$')
 plt.legend(loc='best')
 plt.grid()
+
+
 plt.figure(2)
 plt.plot(x,difvec(x),label='Function is not differentiable at x = 4')
 plt.xlabel('$x$')
