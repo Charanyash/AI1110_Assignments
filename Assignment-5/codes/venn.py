e99e1180cdda02af570a1e244858ed3470d33817
Charanyash <102616426+Charanyash@users.noreply.github.com>
2022-05-23 01:17:15 +0530

    Delete Assignment-5/codes directory

--- Assignment-5/codes/venn.py
+++
@@ -1,8 +0,0 @@
-from matplotlib_venn import venn3, venn3_circles
-from matplotlib import pyplot as plt
-
-
-venn3(subsets=(30,30,10,20,6,4,5), set_labels =('Event A','Event B','Event C'),alpha = 0.5)
-
-
-plt.show()
